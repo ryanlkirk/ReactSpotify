83932f429b6f4f27b2485cce559a37b0a2f59b7d
Ryan Kirk <ryanlkirk@live.co.uk>
2023-05-23 19:43:05 +0100

    Add play button to album

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,11 +1,38 @@
+import React, { useState } from 'react';
+
 const AlbumCard = () => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseOver = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseOut = () => {
+        setIsHovered(false);
+    };
+
     return (
-        <div className="albumCard">
-            <div className="album"></div>
+        <div
+            className="albumCard"
+            onMouseOver={handleMouseOver}
+            onMouseOut={handleMouseOut}
+        >
+            <div className='album'>
+
+                <div className={`playBtn ${isHovered ? '' : 'hide'}`}>
+                    <img
+                        draggable="false"
+                        src={require('../images/ui-elements/play-dark.svg').default}
+                        alt="play"
+                        width="22px"
+                        height="22px"
+                    />
+                </div>
+            </div>
             <p>Peaceful Piano</p>
             <p className="info">Peaceful piano to help you slow down...</p>
         </div>
-    )
-}
+    );
+};
 
 export default AlbumCard;
